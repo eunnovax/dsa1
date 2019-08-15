2eb1d3a593e2a8def8741e37e0f6085efef5729a
Max Plank <darkhan.r4industry@gmail.com>
2019-08-15 20:46:08 +0600

    OOP started

---
+++ OOP.js
@@ -0,0 +1,40 @@
+//1. Create a Basic JavaScript Object
+let dog = {
+  name: 'Dog name',
+  numLegs: 4
+};
+
+//2. Use Dot Notation to Access the Properties of an Object
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+// Add your code below this line
+console.log(dog.name)
+console.log(dog.numLegs)
+
+//3. Create a Method on an Object
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {return "This dog has 4 legs."}
+  
+};
+
+dog.sayLegs();
+
+//4. Make Code More Reusable with the this Keyword
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+};
+
+dog.sayLegs();
+
+//5. Define a Constructor Function
+function Dog() {
+  this.name = "Pet";
+  this.color = "black";
+  this.numLegs = 4;
+}
