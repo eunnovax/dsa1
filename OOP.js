231f486225b05ebee46ecdfa90cbaa7e1497d542
Max Plank <darkhan.r4industry@gmail.com>
2019-08-17 13:06:19 +0600

    OOP continued

--- OOP.js
+++ OOP.js
@@ -38,3 +38,151 @@ function Dog() {
   this.color = "black";
   this.numLegs = 4;
 }
+
+//6. Use a Constructor to Create Objects
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Add your code below this line
+let hound = new Dog();
+
+//7. Extend Constructors to Receive Arguments
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 4;
+}
+
+let terrier = new Dog("Shady", "orange");
+
+//8. Verify an Object's Constructor with instanceof
+/* jshint expr: true */
+
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+// Add your code below this line
+let myHouse = new House(2);
+myHouse instanceof House;
+
+//9. Understand Own Properties
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Add your code below this line
+for (let property in canary) {
+  if(canary.hasOwnProperty) {
+    ownProps.push(property);
+  }
+}
+
+//10. Use Prototype Properties to Reduce Duplicate Code
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 3;
+
+// Add your code above this line
+let beagle = new Dog("Snoopy");
+
+//11. Iterate Over All Properties
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Add your code below this line 
+for (let property in beagle) {
+  if (beagle.hasOwnProperty(property)) {
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
+
+//12. Understand the Constructor Property
+function Dog(name) {
+  this.name = name;
+}
+
+// Add your code below this line
+function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//13. Change the Prototype to a New Object
+function Dog(name) {
+  this.name = name; 
+}
+
+Dog.prototype = {
+  // Add your code below this line
+  numLegs: 2,
+  eat: function() {
+    console.log("eating")
+  },
+  describe: function() {
+    console.log("I am" + this.name + "I have" + this.numLegs + "legs")
+  }
+};
+
+//14. Remember to Set the Constructor Property when Changing the Prototype
+function Dog(name) {
+  this.name = name; 
+}
+
+// Modify the code below this line
+Dog.prototype = {
+  constructor: Dog,
+  numLegs: 2, 
+  eat: function() {
+    console.log("nom nom nom"); 
+  }, 
+  describe: function() {
+    console.log("My name is " + this.name); 
+  }
+};
+
+//15. Understand Where an Object’s Prototype Comes From
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+// Add your code below this line
+Dog.prototype.isPrototypeOf(beagle);
+
+//16. Understand the Prototype Chain
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // => true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
+
+//17. Use Inheritance So You Don't Repeat Yourself
+
+
