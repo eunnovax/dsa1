e19afca3813af258b7cfb6f2b32600e78d216763
Max Plank <darkhan.r4industry@gmail.com>
2019-08-18 00:51:54 +0600

    completed OOP tutorial

--- OOP.js
+++ OOP.js
@@ -227,5 +227,132 @@ let beagle = Object.create(Animal.prototype); // Change this line
 duck.eat(); // Should print "nom nom nom"
 beagle.eat(); // Should print "nom nom nom" 
 
+//19. Set the Child's Prototype to an Instance of the Parent
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Add your code below this line
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
+beagle.eat();  // Should print "nom nom nom"
+
+//20. Reset an Inherited Constructor Property
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Add your code below this line
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duck = new Bird();
+let beagle = new Dog();
+
+//21. Add Methods After Inheritance
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Add your code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function() {
+    console.log("Woof!");
+}
+// Add your code above this line
+
+let beagle = new Dog();
+
+beagle.eat(); // Should print "nom nom nom"
+beagle.bark(); // Should print "Woof!"
+
+//22. Override Inherited Methods
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Add your code below this line
+Penguin.prototype.fly = function() {
+    return "Alas, this is a flightless bird.";
+}
+
+
+// Add your code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+
+//23. Use a Mixin to Add Common Behavior Between Unrelated Objects
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+// Add your code below this line
+let glideMixin = function(obj) {
+    obj.glide = function() {
+        console.log("Gliding!");
+    }
+}
+
+glideMixin(bird);
+glideMixin(boat);
+
+
+//24. Use Closure to Protect Properties Within an Object from Being Modified Externally
+function Bird() {
+  let weight = 15;
+  this.getWeight = function() {
+    return weight;
+  }
+  
+}
+
+//25. Understand the Immediately Invoked Function Expression (IIFE)
+(function () {
+  console.log("A cozy nest is ready");
+})();
+
+//26. Use an IIFE to Create a Module
+let funModule = (function () {
+  return {
+    isCuteMixin: function(obj) {
+  obj.isCute = function() {
+    return true;
+    };
+  },
+  singMixin: function(obj) {
+  obj.sing = function() {
+    console.log("Singing to an awesome tune");
+  };
+  }
+  }
+}) ();
+
+
+
 
 
