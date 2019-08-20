aeddf469ce9f8dbefb6cb2b3ff38cfc496ef55f1
Max Plank <darkhan.r4industry@gmail.com>
2019-08-20 21:51:51 +0600

    started functional programming tutorial

---
+++ FuncProgramming.js
@@ -0,0 +1,147 @@
+//1. Learn About Functional Programming
+/**
+ * A long process to prepare tea.
+ * @return {string} A cup of tea.
+ **/
+const prepareTea = () => 'greenTea';
+
+/**
+ * Get given number of cups of tea.
+ * @param {number} numOfCups Number of required cups of tea.
+ * @return {Array<string>} Given amount of tea cups.
+ **/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+  
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+
+  return teaCups;
+};
+
+// Add your code below this line
+
+const tea4TeamFCC = getTea(40); // :(
+
+// Add your code above this line
+
+console.log(tea4TeamFCC);
+
+//2. Understand Functional Programming Terminology
+/**
+ * A long process to prepare green tea.
+ * @return {string} A cup of green tea.
+ **/
+const prepareGreenTea = () => 'greenTea';
+
+/**
+ * A long process to prepare black tea.
+ * @return {string} A cup of black tea.
+ **/
+const prepareBlackTea = () => 'blackTea';
+
+/**
+ * Get given number of cups of tea.
+ * @param {function():string} prepareTea The type of tea preparing function.
+ * @param {number} numOfCups Number of required cups of tea.
+ * @return {Array<string>} Given amount of tea cups.
+ **/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+
+  return teaCups;
+};
+
+// Add your code below this line
+
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27); // :(
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13); // :(
+
+// Add your code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+
+//3. Understand the Hazards of Using Imperative Code
+
+
+//4. Avoid Mutations and Side Effects Using Functional Programming
+// the global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Add your code below this line
+  let value = fixedValue;
+  value ++;
+  console.log(value);
+  return value;
+  // Add your code above this line
+}
+
+var newValue = incrementer(); // Should equal 5
+console.log(fixedValue); // Should print 4
+
+//5. Pass Arguments to Avoid External Dependence in a Function
+// the global variable
+var fixedValue = 4;
+
+// Add your code below this line
+function incrementer (fvalue) {
+  let value = fvalue;
+  value++;
+  return value;
+  // Add your code above this line
+}
+
+var newValue = incrementer(fixedValue); // Should equal 5
+console.log(fixedValue); // Should print 4
+
+//6. Refactor Global Variables Out of Functions
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function add (arr, bookName) {
+  let newArr = [...arr];  // Copy the bookList array to a new array.
+  newArr.push(bookName);  // Add bookName parameter to the end of the new array.
+  return newArr; // Return the new array.
+}
+
+function remove (arr, bookName) {
+  let newArr = [...arr];  // Copy the bookList array to a new array.
+  if (newArr.indexOf(bookName) >= 0) {   // Check whether the bookName parameter is in new array.
+    newArr.splice(newArr.indexOf(bookName), 1); // Remove the given paramater from the new array.
+    return newArr; // Return the new array.
+    }
+}
+
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
+
+//ALTERNATE SOLUTION
+function add (list,bookName) {
+  return [...list, bookName];
+}
+
+function remove (list,bookName) {
+  if (list.indexOf(bookName) >= 0) {
+    return list.filter((item) => item !== bookName);
+    }
+}
+
